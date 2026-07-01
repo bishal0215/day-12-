45a492ab908a731a64bc3b5b7417cf875ad12d3e
bishal <itsmebishal35@gmail.com>
2026-07-01 08:06:08 +0545

    polymorphism

--- class14.py
+++ class14.py
@@ -1,2 +1,11 @@
-#file handeling 
+#polymorphism is an ability in opp to use a common interface for multiple forms data types 
+class Animals:
+    def speak(self):
+        print("animals makes sounds")
+class cat(Animals):
+    def speak(self):
+        print("cat meow")
+class birds(Animals):
+    def speak(self):
+        print("birds chirps")
 
