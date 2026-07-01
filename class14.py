bb2d8d24564dd00319c31d01c7f94716c936e43e
bishal <itsmebishal35@gmail.com>
2026-07-01 08:15:52 +0545

    code

--- class14.py
+++ class14.py
@@ -9,3 +9,24 @@ class birds(Animals):
     def speak(self):
         print("birds chirps")
 
+#demonstration polymorphism 
+
+
+#inheritance + polymorphism 
+
+class vehicle:
+    def move(self):
+        print("vehicle is moving")
+
+class car(vehicle):
+    def move(self):
+        print("car is moving")
+
+class boat(vehicle):
+    def move(self):
+        print("boat is sailing")
+class aeroplane(vehicle):
+    def move(self):
+        print("Aeroplane is flying")
+
+vehicle = [car(), boat(), aeroplane()]
