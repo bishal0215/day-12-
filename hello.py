cc42d2a71caee1752b6957dc9256737d0514fa6f
bishal <itsmebishal35@gmail.com>
2026-07-03 11:48:16 +0545

    fibonacci

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-name=input("Entet students name :")
+def fibo(n):
+    if n<=1:
+       return n
+    return fibo(n-1) + fibo(n-2)
+def fibonacci(n):
+    return [fibo(i) for i in range(n+1)]
+
+print(fibonacci(5))
+
--- recursivefunction.py
+++ recursivefunction.py
@@ -9,3 +9,26 @@ def countdown(n):
   
 
 countdown(5)
+
+#recursive function to calculate factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(4))
+
+#recursive function to calculate the sum of a list
+def sum_list(lst):
+    if len(lst) == 0:
+        return 0
+    return lst[0] + sum_list(lst[1:])
+print(sum_list([1, 2, 3, 4]))
+
+def fibonacci(n):
+    if n<=1:
+       return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(5))
